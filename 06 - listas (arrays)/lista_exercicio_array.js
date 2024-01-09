389b375980edbdf15ab5cfa19ff47b0917f935b2
brunosilvafreitass <brunodasilvafreitas@outlook.com>
2024-01-09 14:43:19 -0300

    push

--- 06 - listas (arrays)/lista_exercicio_array.js
+++ 06 - listas (arrays)/lista_exercicio_array.js
@@ -1,6 +1,20 @@
 // 1) Crie um programa que dado um número imprima a sua tabuada.
 
+const numero = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${numero} X ${i} = ${i * numero}`);
+}
+
 // 2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.
+
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numerosPares = [];
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 === 0) {
+    numerosPares.push(array[i]);
+  }
+}
+console.log("Números Pares: " + numerosPares.join(", "));
 
 // 3) Faça um programa que dado uma lista de nomes imprima os nomes que começem com a letra "V".
 
