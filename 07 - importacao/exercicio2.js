716557f3e81d216e2b2361492c3ed5225cb1832d
brunosilvafreitass <brunodasilvafreitas@outlook.com>
2024-01-11 00:28:11 -0300

    Atualizar

---
+++ 07 - importacao/exercicio2.js
@@ -0,0 +1,32 @@
+// 2) Faça um programa que receba a quantidade de números e seus respectivos valores.
+// Imprima o maior e o menor número deste conjunto.
+
+// Exemplo:
+//     Entrada:
+//         4
+//         100
+//         150
+//         90
+//         200
+
+//     Saída:
+//         Maior número: 200
+//         Menor número: 90
+
+const { gets, print } = require("./funcoes-auxiliares");
+
+const numeros = gets();
+let maiorNumero = 0;
+let menorNumero = 0;
+
+for (let i = 0; i < numeros; i++) {
+  const numero = gets();
+  if (numero < maiorNumero) {
+    menorNumero = numero;
+  }
+  if (numero > maiorNumero) {
+    maiorNumero = numero;
+  }
+}
+print("Maior número: " + maiorNumero);
+print("Menor número: " + menorNumero);
